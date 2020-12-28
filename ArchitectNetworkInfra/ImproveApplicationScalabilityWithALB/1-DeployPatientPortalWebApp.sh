1166d19759df13a8d8b461b2cc6d084c3cf13e5a
Kishan Salgado <ksalgado@ksalgado-mn2.linkedin.biz>
2020-12-28 15:09:48 -0500

    Update file

---
+++ ArchitectNetworkInfra/ImproveApplicationScalabilityWithALB/1-DeployPatientPortalWebApp.sh
@@ -0,0 +1,10 @@
+### Deploy the patient portal web application ###
+
+#Clone repo
+git clone https://github.com/MicrosoftDocs/mslearn-improve-app-scalability-resiliency-with-load-balancer.git
+cd mslearn-improve-app-scalability-resiliency-with-load-balancer
+
+#Deploy two VMs:
+bash create-high-availability-vm-with-sets.sh learn-c46f0c7d-f5e4-475f-b551-b47914e98e58
+
+
